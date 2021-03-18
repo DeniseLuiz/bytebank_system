87e210e241e2daace296a5e8a86e6b82978e24b2
DeniseLuiz <denisesantos.luiz@usp.br>
2021-03-17 21:19:24 -0300

    get e set

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,79 @@
+﻿namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        private double _saldo = 100;
+        private Cliente _titular;
+
+        public Cliente Titular
+        {
+            get
+            {
+                return _titular;
+            }
+            set
+            {
+                _titular = value;
+            }
+        }
+        public string Agencia { get; set; }
+
+        public string Numero { get; set; }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value; // o value faz referência ao saldo da classe
+            }
+        }
+       
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+
+    }
+
+
+}
+
+
