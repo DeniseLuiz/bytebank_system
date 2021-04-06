0b362182e439bfd9bf71b9f5fb65ffddc5d851bf
DeniseLuiz <denisesantos.luiz@usp.br>
2021-04-06 10:52:15 -0300

    OperacaoNaoRealizadaException

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,11 +10,24 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            //TestarInnerExcepion();
+            CarregarContas();
+            Console.WriteLine("Execução finalizada! Tecle enter para sair");
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+
+        }
+
+        private static void TestarInnerExcepion()
+        {
             try
             {
                 ContaCorrente conta = new ContaCorrente(7564, 98594);
                 Console.WriteLine("Saldo: " + conta.Saldo);
-                //conta.Sacar(-500);
+                //conta.Sacar(-500); //Entre na OperaçãoFinanceiraException
                 ContaCorrente conta2 = new ContaCorrente(12345, 5553);
                 Console.WriteLine("Saldo2: " + conta2.Saldo);
                 conta.Transferir(5000, conta2);
@@ -22,7 +35,7 @@ namespace ByteBank
                 //ContaCorrente conta = new ContaCorrente(0, 0); //entra na ArgumentException
                 Console.WriteLine("taxa de operação " + ContaCorrente.TaxaDeoperacao);
             }
-            catch(SaldoInsuficienteException ex)
+            catch (SaldoInsuficienteException ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -40,8 +53,6 @@ namespace ByteBank
                 Console.WriteLine(ex.StackTrace);
                 Console.WriteLine(ex.InnerException.Message);
             }
-            
-            Console.ReadLine();
         }
     }
 }
